76a5d17d4d847e99e29fd2ce7fbf12be9075eb6d
@hexlet/cli <support@hexlet.io>
2024-07-31 23:56:28 +0500

    submit rails-basics-ru/quality

---
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  self.use_transactional_tests = true
+  test 'opens bulletins' do
+    get bulletins_url
+    assert_response :success
+    assert_select 'h1', 'Bulletins'
+  end
+
+  test 'opens one bulletin' do
+    bulletin = bulletins :bulletin1
+    get bulletin_path bulletin
+
+    assert_response :success
+    assert_select 'h1', 'Bull1'
+    assert_select 'p', 'Bull1_body'
+  end
+end
