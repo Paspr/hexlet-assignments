fbc25810aa93a430e7829cb12938192e06d7da5b
@hexlet/cli <support@hexlet.io>
2024-08-06 07:49:47 +0500

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/db/seeds.rb
+++ rails-basics-ru/crud/db/seeds.rb
@@ -7,7 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |index|
+10.times do |_index|
   Task.create(
     name: Faker::Name.name,
     description: Faker::Books::Dune.planet,
